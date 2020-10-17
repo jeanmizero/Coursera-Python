5cd9f006f28ce36e67d76356410ac3c5900fb558
Jean <mizero2@gmail.com>
2020-10-17 00:31:25 -0600

    Add one more warm up

--- week_4/main.py
+++ week_4/main.py
@@ -2,3 +2,9 @@
 # Prompt the user input
 name = input("Enter your name")
 print("Hello", name)
+
+# Float input
+hrs = input("Enter hours")
+rate = input("Enter rate per hour")
+pay = float(hrs) * float(rate)
+print("Pay:", pay)
